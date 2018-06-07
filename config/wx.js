7db2379e678af7da3695e320d651e1ec3ef1c6be
PrinnyRay <455321557@qq.com>
2018-06-07 13:42:43 +0800

    添加wx配置，验证接入

---
+++ config/wx.js
@@ -0,0 +1,7 @@
+var wx = {};
+
+wx.url = 'http://118.25.178.109/';
+wx.token = 'wrsndm';
+wx.EncodingAESKey = 'zUk0z8Cna6lqnNguPpQotrGm9iKC2QKcFVXkBQWUiGp';
+
+module.exports = wx;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var wx = require('../config/wx');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var token = wx.token;
+  var signature = req.query.signature;
+  var nonce = req.query.nonce;
+  var timestamp = req.query.timestamp;
+  var echostr = req.query.echostr;
+  var str = [token, timestamp, nonce].sort().join('')
+  var sha = sha1(str);
+
+  if(sha === signature) {
+    res.send(echostr + '');
+  } else {
+    res.send('error');
+  }
 });
 
 module.exports = router;
