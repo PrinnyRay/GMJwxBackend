a032e0dacd2ce7afadd0e5b8c7a03c908986f39b
PrinnyRay <455321557@qq.com>
2018-06-14 07:05:32 +0800

    添加一个测试

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,24 @@ var wx = require('../config/wx');
 var crypto = require('crypto');
 var wechat = require('wechat');
 
-router.use('/wechat', wechat(wx.config, (req, res, next) => {
-  if(req.weixin.content === '随便看看') {
-    res.reply('呵呵');
-  }
+router.use('/', wechat(wx.config, (req, res, next) => {
+  res.reply('呵呵');
 }));
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var token = wx.token;
-  var signature = req.query.signature;
-  var nonce = req.query.nonce;
-  var timestamp = req.query.timestamp;
-  var echostr = req.query.echostr;
-  var str = [token, timestamp, nonce].sort().join('');
-  var sha = crypto.createHash('sha1').update(str).digest('hex');
+// router.get('/', function(req, res, next) {
+//   var token = wx.token;
+//   var signature = req.query.signature;
+//   var nonce = req.query.nonce;
+//   var timestamp = req.query.timestamp;
+//   var echostr = req.query.echostr;
+//   var str = [token, timestamp, nonce].sort().join('');
+//   var sha = crypto.createHash('sha1').update(str).digest('hex');
 
-  if(sha === signature) {
-    res.send(echostr + '');
-  } else {
-    res.send('error');
-  }
-});
+//   if(sha === signature) {
+//     res.send(echostr + '');
+//   } else {
+//     res.send('error');
+//   }
+// });
 
 module.exports = router;
