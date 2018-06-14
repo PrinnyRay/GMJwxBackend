1a24a0bc565ce91c86658b5a7231123de5e20ce7
PrinnyRay <455321557@qq.com>
2018-06-14 10:56:18 +0800

    菜单

--- routes/wechat.js
+++ routes/wechat.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var wechat = require('wechat');
 var movie = require('../models/movie');
 var parse = require('../utils/parseInfo');
+var wechatapi = require('wechat-api');
+var menu = require('../config/menu.json');
+var Event = require('wechat').Event;
 
 var config = {
   token: 'wrsndm',
@@ -11,9 +14,30 @@ var config = {
   encodingAESKey: 'K0ePPEb896e6GjTRsc852VHwpwm7uDHcr3tyXUMETpY',
   checkSignature: false
 };
+var api = new wechatapi(config.appid, config.appsecret);
+api.createMenu(menu, () => {
+  console.log('菜单初始化成功');
+});
+var events = new Event();
+events.add('menu_btn_random', (req, res, next) => {
+  movie.count({}, (err, c) => {
+    movie.find({}, (err, doc) => {
+      moviedoc = doc[parseInt(Math.random() * c)];
+      res.reply([
+        {
+          title: moviedoc.title,
+          description: parse(moviedoc),
+          picurl: moviedoc.cover,
+          url: 'https://movie.douban.com/subject/'+moviedoc.id
+        }
+      ]);
+    });
+  });
+});
+var handle = Event.dispatch(events);
 
 router.use(express.query());
-
+router.use('/', wechat(config).event(handle).middlewarify());
 router.use('/', wechat(config, (req, res, next) => {
   var message = req.weixin;
   if(message.Content === '随便看看') {
@@ -28,8 +52,8 @@ router.use('/', wechat(config, (req, res, next) => {
             url: 'https://movie.douban.com/subject/'+moviedoc.id
           }
         ]);
-      })
-    })
+      });
+    });
   } else {
     res.reply('111');
   }
