f13a9b3a7d9c04acae0533cfb38e88feaa6218ed
PrinnyRay <455321557@qq.com>
2018-06-15 12:14:04 +0800

    222

--- utils/parseInfo.js
+++ utils/parseInfo.js
@@ -1,8 +1,9 @@
 var parseInfo = {
 	'parse' : function(movie) {
 		res = "";
-		res += "影片名： " + movie.title + " / " + movie.titleEng + "\n";
-		res += "上映年份： " + movie.year + "\n"; 
+		res += "影片名： " + movie.title + " / " + movie.titleEng + " / " + movie.alias.join(" / ") + "\n";
+		res += "上映年份： " + movie.year + "\n";
+		res += "评分： " + movie.rate + "\n";
 		res += "国家： " + movie.countries.join(" / ") + "\n";
 		res += "分类： " + movie.categories.join(" / ") + "\n";
 		res += "导演： " + movie.director + "\n";
